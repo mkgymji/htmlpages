06ee1b1b67b4a6be8f5d9ba4d30b13b373ca9f62
Michal Kočer <65507427+mkgymji@users.noreply.github.com>
2024-12-10 12:59:11 +0000

    Refactor JavaScript code to improve variable initialization, enhance greeting function, and add event listeners for interactive elements

--- javascript/skript.js
+++ javascript/skript.js
@@ -1,5 +1,6 @@
 // Příklad skriptu v programovacím jazyce Javascript
-var jmeno =  "Jiří";
+var jmeno;
+jmeno =  "Jiří";
 var cislo =   3;
 var pi =  3.14;
 
@@ -9,9 +10,46 @@ console.log(jmeno)
 console.log(cislo)
 console.log(dvepi)
 
-// alert("Ahoj")
+//  alert("Ahoj")
 
 function greeting(name) {
-    alert("Ahoj " + name );
+    console.log("Ahoj " + name );
 }
-greeting("Johny");
+greeting(jmeno);
+
+// -----------------------------------------
+function hlavni(event) {
+    var pocitadlo = 0;
+    console.log("Hlavni funkce ")
+    console.log(event);
+    // SELECTORY
+    var nadpis = document.querySelector("h1");
+    var vstup = document.querySelector("#vstup");
+    var odstavec = document.querySelector("#prazdny")
+    var tlCount= document.querySelector("#tl1")
+    var tlReset = document.querySelector("#tl2")
+    // EVENT LISTENERS
+    vstup.addEventListener("change", function(event){
+        pocitadlo = parseInt(event.target.value);
+        if (isNaN(pocitadlo)) {
+            pocitadlo = 0;
+        }
+        odstavec.textContent= "Pocitadlo: " + pocitadlo;
+    });
+    tlCount.addEventListener("click", function(event){
+        pocitadlo = pocitadlo + 1;
+        odstavec.textContent= "Pocitadlo: " + pocitadlo;
+    });
+    tlReset.addEventListener("click", function(event){
+        pocitadlo = 0;
+        odstavec.textContent= "Pocitadlo: " + pocitadlo;
+    });
+    // Změna objektu
+    nadpis.style.color = "red"
+    nadpis.textContent = "Učíme se Javascript"
+    odstavec.innerHTML = "<ul><li>prvni<li>druhy</ul"
+
+}
+// -----------------------------------------
+
+document.addEventListener("DOMContentLoaded", hlavni);  
